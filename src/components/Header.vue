813b6b0bdefa57d7788eadcadd086a98f00d520c
Ala <alasbed+git@gmail.com>
2021-01-19 14:54:30 +0100

    shopping cart popup no show + item count in cart no work

---
+++ src/components/Header.vue
@@ -0,0 +1,83 @@
+<template>
+<header>
+  <img src="@/assets/graphics/graphics-header.svg" alt="header" />
+  <div class="navMenu">
+      <img src="@/assets/graphics/navicon.svg" alt="nav" class="navImg"
+           @click="showMenu"/></div>
+  <div class="bagMenu"
+       v-if="showCartIcon"
+       v-on:visible="visible">
+    <span class="badge">{{cartCount}}</span>
+    <img src="@/assets/graphics/bag.svg" alt="bag" class="bagImg"
+         @click="cart"/>
+  </div>
+</header>
+</template>
+
+<script>
+export default {
+  name: "Header",
+  components: {},
+  props: {
+    showCartIcon: Boolean,
+    cartCount: Number
+  },
+  methods: {
+    showMenu() {
+      this.$router.push('Navigation')
+    },
+    visible() {
+      this.showNav = !this.showNav;
+    },
+  cart() {
+      this.$root.$emit('showCart')
+    console.log('cart clicked')
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+.navMenu {
+  border-radius: 100% !important;
+  height: 45px;
+  width: 45px;
+  position: absolute;
+  margin-top: -100px;
+  margin-left: 15px;
+  background-color: white;
+}
+
+.navImg {
+  margin-top: 12px;
+  margin-left: 10px;
+}
+
+.bagImg {
+  margin-top: -7px;
+  margin-left: 14px;
+}
+
+.bagMenu {
+  border-radius: 100% !important;
+  height: 45px;
+  width: 45px;
+  position: absolute;
+  margin-top: -100px;
+  margin-left: 315px;
+  background-color: black;
+}
+
+.badge {
+  border-radius: 100%;
+  box-shadow: 0.15rem 0.15rem 0 rgba(0, 0, 0, 0.1);
+  border: 1px solid transparent;
+  padding: 0.375rem 0.75rem;
+  font-size: 1rem;
+  text-align: start;
+  margin-left: 1.8rem;
+  background-color: rgb(229, 103, 78);
+  color: white;
+}
+</style>
---
+++ src/views/Status.vue
@@ -0,0 +1,80 @@
+<template>
+  <section class="status">
+    <div class="orderNumber">Ordernummer: {{ orderNr }}</div>
+    <img src="@/assets/graphics/drone.svg" class="drone" alt="drone"/>
+    <div class="order">
+      <span>Din beställning</span><br/>
+      <span>är på väg!</span>
+    </div>
+    <div class="timer">{{ETA}} minuter</div>
+    <a class="tag" @click="returnMenu">Ok, cool!</a>
+  </section>
+</template>
+
+<script>
+import { generateOrderNr, generateETA } from "@/backend/utils/utils.js";
+
+export default {
+  name: "Menu",
+  components: {
+  },
+  data() {
+    return {
+      orderNr: "",
+      ETA: 0,
+    }
+  },
+  methods: {
+    getData() {
+      this.orderNr = generateOrderNr();
+      this.ETA = generateETA();
+    },
+    returnMenu() {
+      this.$router.push('Menu')
+    }
+  },
+  async mounted() {
+    this.getData();
+  }
+}
+</script>
+
+<style scoped>
+.status {
+  background-color: #e26b53;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+.orderNumber {
+  color: #f1d6d3;
+  letter-spacing: 1px;
+  margin-top: 50px;
+  margin-bottom: 50px;
+}
+.order {
+  color: white;
+  font-family: serif;
+  font-size: 2.2rem;
+  text-align: center;
+  font-weight: 600;
+  margin-top: 50px;
+  margin-bottom: 15px;
+}
+.timer {
+  color: white;
+  font-family: serif;
+  font-size: 1rem;
+  letter-spacing: 1px;
+  font-weight: 600;
+}
+.tag {
+  background-color: white;
+  padding: 10px 30px 10px 30px;
+  margin: 75px;
+  border-radius: 20px;
+  font-family: serif;
+  font-size: 1.5rem;
+  font-weight: bold;
+}
+</style>
